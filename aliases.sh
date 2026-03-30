eb448fc19291e64d3d08946ae7f1f9557b4646a4
Chiel Fernhout <fernhoutchiel@gmail.com>
2026-03-30 14:29:45 +0200

    Simplify aliases using new flags

--- aliases.sh
+++ aliases.sh
@@ -2,11 +2,6 @@
 # Add to your .bashrc, .zshrc, or aliases file:
 #   source /path/to/aliases.sh
 
-# Read a file aloud: tts file.md, tts -s 1.5 file.md
-alias tts='read-to-me'
-
-# Read clipboard aloud: cts, cts -s 1.3
-cts() { xclip -selection clipboard -o | read-to-me "$@"; }
-
-# Read clipboard with extra markdown stripping: mts, mts -s 1.5
-mts() { xclip -selection clipboard -o | sed 's/[#*`_~>]//g; s/\[.*\](.*)//g; s/^-\s//g' | read-to-me "$@"; }
+alias tts='read-to-me'           # tts file.md, tts -s 1.5 file.md
+alias cts='read-to-me -c'        # cts, cts -s 1.3
+alias mts='read-to-me -c -m'     # mts, mts -s 1.5
